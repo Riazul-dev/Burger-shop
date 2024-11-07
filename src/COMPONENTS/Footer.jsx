b198daf08ab1671b8253322b270b9cb7dbb65d69
”Riazul-dev” <riazul4u@gmail.com>
2024-11-07 14:58:33 +0600

    edit footer css

--- src/COMPONENTS/Footer.jsx
+++ src/COMPONENTS/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div className='h-[10vh] bg-slate-400 text-white flex justify-center items-center text-center'>
+    <div className='h-[20vh] lg:h-[10vh] bg-slate-400 text-white flex justify-center items-center text-center'>
       <p>Copyright &copy; {new Date().getFullYear()} . All rights reserved || This Website is made with <span className='text-xl text-orange-300'>Riazul_Karim</span></p>
     </div>
   )
